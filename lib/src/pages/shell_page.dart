63b3d43f9727d7d37a72bfe9cdc53ee06e0c5a5a
aspiringluke <l.paulino@sou.unifeob.edu.br>
2026-04-07 20:50:21 -0300

    refactor: Shell page refatorada
    Agora a shell page contém a estrutura básica,
    project_list_page contém a listagem de projetos
    e idea_list_page contém listagem de ideias

--- lib/src/pages/shell_page.dart
+++ lib/src/pages/shell_page.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import './ideas_page.dart';
+import './idea_list_page.dart';
+import './project_list_page.dart';
 import '../widgets/custom_nav_bar.dart';
 import '../widgets/funcoes_busca.dart';
 import '../widgets/glass_circle_button.dart';
 import '../widgets/main_header.dart';
-import '../widgets/project_card.dart';
 
 class ShellPage extends StatefulWidget {
   final NavTab initialTab;
@@ -122,8 +121,7 @@ class _AnimatedTabContent extends StatelessWidget {
           fit: StackFit.expand,
           children: [
             ...previousChildren,
-            // ignore: use_null_aware_elements
-            if (currentChild != null) currentChild,
+            ?currentChild,
           ],
         );
       },
@@ -136,12 +134,8 @@ class _AnimatedTabContent extends StatelessWidget {
           parent: directionAwareAnimation,
           curve: Curves.easeOutCubic,
         );
-        final beginX = isIncoming
-            ? (toIdeas ? 20.0 : -20.0)
-            : 0.0;
-        final endX = isIncoming
-            ? 0.0
-            : (toIdeas ? -20.0 : 20.0);
+        final beginX = isIncoming ? (toIdeas ? 20.0 : -20.0) : 0.0;
+        final endX = isIncoming ? 0.0 : (toIdeas ? -20.0 : 20.0);
 
         final offsetAnimation = Tween<double>(
           begin: beginX,
@@ -169,178 +163,9 @@ class _AnimatedTabContent extends StatelessWidget {
       child: KeyedSubtree(
         key: ValueKey(activeTab),
         child: activeTab == NavTab.projects
-            ? const _ProjectsContent()
+            ? const ProjectListPage()
             : const IdeasContent(),
       ),
     );
   }
-}
-
-class _ProjectsContent extends StatefulWidget {
-  const _ProjectsContent();
-
-  @override
-  State<_ProjectsContent> createState() => _ProjectsContentState();
-}
-
-class _ProjectsContentState extends State<_ProjectsContent> {
-  late List<_ProjectListItem> _projects;
-
-  bool get _isMobileReorderEnabled {
-    if (kIsWeb) return false;
-
-    return switch (defaultTargetPlatform) {
-      TargetPlatform.android || TargetPlatform.iOS => true,
-      _ => false,
-    };
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _projects = List.generate(
-      4,
-      (index) => _ProjectListItem(
-        title: 'Projeto ${index + 1}',
-        unpinnedIndex: index,
-      ),
-    );
-  }
-
-  void _togglePinned(_ProjectListItem project) {
-    setState(() {
-      final currentIndex = _projects.indexOf(project);
-      if (currentIndex == -1) return;
-
-      if (!project.isPinned) {
-        project.unpinnedIndex = _unpinnedIndexAt(currentIndex);
-      }
-
-      _projects.removeAt(currentIndex);
-      project.isPinned = !project.isPinned;
-
-      if (project.isPinned) {
-        _projects.insert(0, project);
-      } else {
-        final pinnedCount = _projects.where((item) => item.isPinned).length;
-        final unpinnedCount = _projects.length - pinnedCount;
-        final targetUnpinnedIndex = project.unpinnedIndex.clamp(0, unpinnedCount) as int;
-        _projects.insert(pinnedCount + targetUnpinnedIndex, project);
-        _updateUnpinnedSlots();
-      }
-    });
-  }
-
-  int _unpinnedIndexAt(int listIndex) {
-    var count = 0;
-
-    for (var index = 0; index < listIndex; index += 1) {
-      if (!_projects[index].isPinned) {
-        count += 1;
-      }
-    }
-
-    return count;
-  }
-
-  void _normalizePinnedGroups() {
-    final pinned = _projects.where((item) => item.isPinned).toList(growable: false);
-    final unpinned = _projects.where((item) => !item.isPinned).toList(growable: false);
-    _projects = <_ProjectListItem>[
-      ...pinned,
-      ...unpinned,
-    ];
-  }
-
-  void _updateUnpinnedSlots() {
-    var unpinnedIndex = 0;
-
-    for (final project in _projects) {
-      if (!project.isPinned) {
-        project.unpinnedIndex = unpinnedIndex;
-        unpinnedIndex += 1;
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Widget buildProjectCard(BuildContext context, int index) {
-      final project = _projects[index];
-      final card = RepaintBoundary(
-        child: ProjectCard(
-          title: project.title,
-          isPinned: project.isPinned,
-          onTogglePinned: () => _togglePinned(project),
-        ),
-      );
-
-      if (_isMobileReorderEnabled) {
-        return ReorderableDelayedDragStartListener(
-          key: ValueKey(project.title),
-          index: index,
-          child: card,
-        );
-      }
-
-      return KeyedSubtree(
-        key: ValueKey(project.title),
-        child: card,
-      );
-    }
-
-    if (!_isMobileReorderEnabled) {
-      return ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        itemCount: _projects.length,
-        itemBuilder: buildProjectCard,
-      );
-    }
-
-    return ReorderableListView.builder(
-      buildDefaultDragHandles: false,
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      itemCount: _projects.length,
-      onReorder: (oldIndex, newIndex) {
-        if (!_isMobileReorderEnabled) return;
-
-        if (newIndex > oldIndex) {
-          newIndex -= 1;
-        }
-        setState(() {
-          final item = _projects.removeAt(oldIndex);
-          _projects.insert(newIndex, item);
-          _normalizePinnedGroups();
-          _updateUnpinnedSlots();
-        });
-      },
-      proxyDecorator: (widget, index, animation) {
-        return Material(
-          elevation: 12,
-          color: Colors.transparent,
-          shadowColor: Colors.pink.withOpacity(0.25),
-          child: Opacity(
-            opacity: 0.95,
-            child: Transform.scale(
-              scale: 1.02,
-              child: widget,
-            ),
-          ),
-        );
-      },
-      itemBuilder: buildProjectCard,
-    );
-  }
-}
-
-class _ProjectListItem {
-  final String title;
-  bool isPinned;
-  int unpinnedIndex;
-
-  _ProjectListItem({
-    required this.title,
-    this.isPinned = false,
-    required this.unpinnedIndex,
-  });
 }
