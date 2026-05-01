facdf049a5288fffec22783b208865d2551a5d76
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2026-05-01 17:43:08 -0300

    change: corrigido dataTypes para adequação ao SQLite

--- ref/sqlite/db_script.sql
+++ ref/sqlite/db_script.sql
@@ -1,61 +1,61 @@
 CREATE TABLE IF NOT EXISTS Tags (
-    idTag INT PRIMARY KEY AUTO_INCREMENT,
+    idTag INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(255),
     cor VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS Diagramas (
-    idDiagrama INT PRIMARY KEY AUTO_INCREMENT,
+    idDiagrama INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Nota (
-    idNota INT PRIMARY KEY AUTO_INCREMENT,
+    idNota INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(255),
-    pastas_idPasta INT,
+    pastas_idPasta INTEGER,
     FOREIGN KEY (pastas_idPasta) REFERENCES Pastas(idPasta)
 );
 
 CREATE TABLE IF NOT EXISTS Pastas (
-    idPasta INT PRIMARY KEY AUTO_INCREMENT,
+    idPasta INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Projeto (
-    idProjeto INT PRIMARY KEY AUTO_INCREMENT,
-    tag_idTag INT,
-    sintese VARCHAR(100),
-    nota_idNota INT,
+    idProjeto INTEGER PRIMARY KEY AUTOINCREMENT,
+    tag_idTag INTEGER,
+    sINTEGERese VARCHAR(100),
+    nota_idNota INTEGER,
     estilo JSON,
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag),
     FOREIGN KEY (nota_idNota) REFERENCES Nota(idNota)
 );
 
 CREATE TABLE IF NOT EXISTS Personagem (
-    idPersonagem INT PRIMARY KEY AUTO_INCREMENT,
+    idPersonagem INTEGER PRIMARY KEY AUTOINCREMENT,
     informacoes_gerais JSON,
     aparencia JSON,
     historia JSON,
     psique JSON,
-    tag_idTag INT,
-    projeto_idProjeto INT,
+    tag_idTag INTEGER,
+    projeto_idProjeto INTEGER,
     FOREIGN KEY (projeto_idProjeto) REFERENCES Projeto(idProjeto),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
 
 CREATE TABLE IF NOT EXISTS Nos (
-    idNos INT PRIMARY KEY AUTO_INCREMENT,
+    idNos INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(100),
-    diagrama_idDiagrama INT,
+    diagrama_idDiagrama INTEGER,
     FOREIGN KEY (diagrama_idDiagrama) REFERENCES Diagramas(idDiagrama)
 );
 
 CREATE TABLE IF NOT EXISTS Aresta (
-    idAresta INT PRIMARY KEY AUTO_INCREMENT,
+    idAresta INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(200),
-    diagrama_idDiagrama INT,
-    id_no_origem INT UNIQUE,
-    id_no_destino INT UNIQUE,
+    diagrama_idDiagrama INTEGER,
+    id_no_origem INTEGER UNIQUE,
+    id_no_destino INTEGER UNIQUE,
     forma_linha ENUM('seccionada', 'continua'),
     forma_ponta_origem ENUM('vazio', 'traingular', 'quadrada', 'circular'),
     forma_ponta_destino ENUM('vazio', 'traingular', 'quadrada', 'circular'),
@@ -65,15 +65,15 @@ CREATE TABLE IF NOT EXISTS Aresta (
 );
 
 CREATE TABLE IF NOT EXISTS nos_has_tags (
-    nos_idNos INT PRIMARY KEY,
-    tag_idTag INT PRIMARY KEY,
+    nos_idNos INTEGER PRIMARY KEY,
+    tag_idTag INTEGER PRIMARY KEY,
     FOREIGN KEY (nos_idNos) REFERENCES Nos(idNos),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
 
 CREATE TABLE IF NOT EXISTS notas_has_tags (
-    notas_idNotas INT PRIMARY KEY,
-    tag_idTag INT PRIMARY KEY,
+    notas_idNotas INTEGER PRIMARY KEY,
+    tag_idTag INTEGER PRIMARY KEY,
     FOREIGN KEY (notas_idNota) REFERENCES Nota(idNota),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
