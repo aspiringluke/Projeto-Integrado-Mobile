3a3d427579c6f0c9b46c041261f93b2e18a7b952
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2026-05-04 16:18:03 -0300

    refactor: reorganizar e corrigir tabelas no script SQL do banco SQLite

--- ref/sqlite/db_script.sql
+++ ref/sqlite/db_script.sql
@@ -9,38 +9,40 @@ CREATE TABLE IF NOT EXISTS Diagramas (
     titulo VARCHAR(100)
 );
 
-
-CREATE TABLE IF NOT EXISTS Pastas (
-    idPasta INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo VARCHAR(100)
-);
-
-CREATE TABLE IF NOT EXISTS Nota (
+CREATE TABLE IF NOT EXISTS Notas (
     idNota INTEGER PRIMARY KEY AUTOINCREMENT,
-    descricao VARCHAR(255),
+    titulo VARCHAR(255),
+    descricao TEXT,
+    cor VARCHAR(15),
     pastas_idPasta INTEGER,
     FOREIGN KEY (pastas_idPasta) REFERENCES Pastas(idPasta)
 );
 
-CREATE TABLE IF NOT EXISTS Projeto (
+CREATE TABLE IF NOT EXISTS Pastas (
+    idPasta INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo VARCHAR(100),
+    cor VARCHAR(15),
+    pastas_idPasta INTEGER,
+    FOREIGN KEY (pastas_idPasta) REFERENCES Pastas(idPasta)
+);
+
+CREATE TABLE IF NOT EXISTS Projetos (
     idProjeto INTEGER PRIMARY KEY AUTOINCREMENT,
     tag_idTag INTEGER,
     sintese VARCHAR(100),
-    nota_idNota INTEGER,
-    estilo TEXT,
-    FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag),
-    FOREIGN KEY (nota_idNota) REFERENCES Nota(idNota)
+    estilo JSON,
+    FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
 
-CREATE TABLE IF NOT EXISTS Personagem (
+CREATE TABLE IF NOT EXISTS Personagens (
     idPersonagem INTEGER PRIMARY KEY AUTOINCREMENT,
-    informacoes_gerais TEXT, 
-    aparencia TEXT,          
-    historia TEXT,           
-    psique TEXT,             
+    informacoes_gerais JSON,
+    aparencia JSON,
+    historia JSON,
+    psique JSON,
     tag_idTag INTEGER,
     projeto_idProjeto INTEGER,
-    FOREIGN KEY (projeto_idProjeto) REFERENCES Projeto(idProjeto),
+    FOREIGN KEY (projeto_idProjeto) REFERENCES Projetos(idProjeto),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
 
@@ -57,26 +59,21 @@ CREATE TABLE IF NOT EXISTS Aresta (
     diagrama_idDiagrama INTEGER,
     id_no_origem INTEGER UNIQUE,
     id_no_destino INTEGER UNIQUE,
-    forma_linha TEXT CHECK(forma_linha IN ('seccionada', 'continua')), 
-    forma_ponta_origem TEXT CHECK(forma_ponta_origem IN ('vazio', 'traingular', 'quadrada', 'circular')),
-    forma_ponta_destino TEXT CHECK(forma_ponta_destino IN ('vazio', 'traingular', 'quadrada', 'circular')),
     FOREIGN KEY (diagrama_idDiagrama) REFERENCES Diagramas(idDiagrama),
     FOREIGN KEY (id_no_origem) REFERENCES Nos(idNos),
     FOREIGN KEY (id_no_destino) REFERENCES Nos(idNos)
 );
 
 CREATE TABLE IF NOT EXISTS nos_has_tags (
-    nos_idNos INTEGER,
-    tag_idTag INTEGER,
-    PRIMARY KEY (nos_idNos, tag_idTag), -
+    nos_idNos INTEGER PRIMARY KEY,
+    tag_idTag INTEGER NOT NULL,
     FOREIGN KEY (nos_idNos) REFERENCES Nos(idNos),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
 
 CREATE TABLE IF NOT EXISTS notas_has_tags (
-    notas_idNota INTEGER, 
-    tag_idTag INTEGER,
-    PRIMARY KEY (notas_idNota, tag_idTag), 
-    FOREIGN KEY (notas_idNota) REFERENCES Nota(idNota),
+    notas_idNotas INTEGER PRIMARY KEY,
+    tag_idTag INTEGER NOT NULL,
+    FOREIGN KEY (notas_idNotas) REFERENCES Notas(idNota),
     FOREIGN KEY (tag_idTag) REFERENCES Tags(idTag)
 );
