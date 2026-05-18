c921200286b9dfbd7126423294dc98cbf6c6d6cf
aspiringluke <l.paulino@sou.unifeob.edu.br>
2026-05-18 14:03:53 -0300

    refactor: Removidos prints no viewmodel do chatbot

--- lib/src/features/chatbot/viewmodels/chatbot_viewmodel.dart
+++ lib/src/features/chatbot/viewmodels/chatbot_viewmodel.dart
@@ -12,28 +12,17 @@ class ChatbotViewModel extends ChangeNotifier {
   List<String> mensagens = [];
 
   Future<void> enviarMensagem(String msg) async {
-
-    print("1 - método iniciou");
-
     if (msg.trim().isEmpty) return;
 
     mensagens.add(msg);
-
     notifyListeners();
 
     List<String> contexto = List.from(mensagens);
     contexto.removeLast();
-
-    print("2 - chamando repository");
-
     final resposta = await repository.enviarMensagem(
       contexto,
       msg,
     );
-
-    print("3 - resposta recebida");
-    print("Sucesso: ${resposta.$1}");
-    print("Texto: ${resposta.$2}");
 
     mensagens.add(
       resposta.$1
